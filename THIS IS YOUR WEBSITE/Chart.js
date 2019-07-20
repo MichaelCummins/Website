fc2bb7511c2dc65d04a94de09dbd7a4343b2b70a
MichaelCummins <Michael.Cummins1@marist.edu>
2019-07-20 19:10:52 -0400

    Added back to top button

--- THIS IS YOUR WEBSITE/Chart.js
+++ THIS IS YOUR WEBSITE/Chart.js
@@ -1,4 +1,11 @@
-
+function scrollToTop() {
+    var position =
+        document.body.scrollTop || document.documentElement.scrollTop;
+    if (position) {
+        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+        scrollAnimation = setTimeout("scrollToTop()", 30);
+    } else clearTimeout(scrollAnimation);
+}
 
 window.onload = function() {
 
